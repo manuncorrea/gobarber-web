fd926ff934c481f822ec74709a1abbcd7b876a1d
Emanuele Correa <manuncorrea@gmail.com>
2020-09-18 09:35:03 -0300

    validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback} from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,9 +13,20 @@ import Button from '../../components/Button';
 import { Container, Content, Background} from './styles';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: object): void {
-        console.log(data);
-    }
+   const handleSubmit = useCallback(async (data: object) => {
+        try{
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatorio'),
+                email: Yup.string().required('E-mail obrigatorio').email('Digite um e-email valido'),
+                password: Yup.string().min(6, 'No minimo 6 digitos'),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        }catch(err) {
+            console.log(err);     
+        }
+    }, []);
     return(
         <Container>
             <Background />

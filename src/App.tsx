2ad311482f56a5884195438b9aec9f91f366d49c
Emanuele Correa <manuncorrea@gmail.com>
2020-09-17 22:07:15 -0300

    pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignUp from './pages/SignUp';
 import SignIn from './pages/Sigin';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );

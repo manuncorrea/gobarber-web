9a04997fbd61fec5524fd5cb9ce9c109e5079791
Emanuele Correa <manuncorrea@gmail.com>
2020-09-24 19:24:05 -0300

    erro local storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -19,12 +19,12 @@ interface AuthContextDate {
 const AuthContext = createContext<AuthContextDate>({} as AuthContextDate);
 
 const AuthProvider: React.FC = ({ children }) => {
-    const [data, setData] = useState<AuthState>(() => {
+    const [ data, setData] = useState<AuthState>(() => {
        const token = localStorage.getItem('@GoBarber:token');
        const user = localStorage.getItem('@GoBarber:user');
 
-       if (token && user) {
-           return { token, user: JSON.parse(user)};
+       if(token && user){
+           return { token, user: JSON.parse(user) };
        }
 
        return {} as AuthState;
@@ -36,10 +36,13 @@ const AuthProvider: React.FC = ({ children }) => {
             password,
         });
 
-       const { token, user } = response.data;
+        const { token, user } = response.data;
 
-       localStorage.setItem('@GoBarber:token', token);
-       localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+        localStorage.setItem('@GoBarber:token', token);
+        localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+        setData({ token, user});
+
     }, []);
 
     return(
@@ -51,4 +54,4 @@ const AuthProvider: React.FC = ({ children }) => {
 
 
 
-export { AuthContext, AuthProvider};
+export { AuthContext, AuthProvider}; 

b062711a028dc5355b11b0dc94fc0c4b65903446
Emanuele Correa <manuncorrea@gmail.com>
2020-09-27 08:43:36 -0300

    criando rotas e aplicando animação nas paginas

--- src/pages/Sigin/styles.ts
+++ src/pages/Sigin/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { shade } from 'polished';
 
@@ -15,12 +15,33 @@ export const Content = styled.div`
     /**Centralizar na vertiaval */
     display: flex;
     flex-direction: column;
-    place-content: center; /*alinhar na vertical*/
-
     align-items: center;
+    justify-content: center;
 
     width: 100%;
-    max-width: 700px;
+    max-width: 700px;   
+`;
+  /*Animação na HOME */
+const appearFromLeft = keyframes`
+    from{
+        opacity: 0;
+        transform: translateX(-50%);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`;
+
+export const AnimationContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    /*Animação na HOME */
+    animation: ${appearFromLeft} 1s;
 
     form{
         margin: 80px 0;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useRef} from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
-
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-
 import * as Yup from 'yup';
+import  { Link } from 'react-router-dom';
+
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,7 +13,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background} from './styles';
+import { Container, Content, AnimationContainer, Background} from './styles';
 
 const SignUp: React.FC = () => {
    const formRef = useRef<FormHandles>(null);
@@ -40,23 +41,25 @@ const SignUp: React.FC = () => {
         <Container>
             <Background />
             <Content>
-                <img src={logoImg} alt="GoBarber" />
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarber" />
 
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu cadastro</h1>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu cadastro</h1>
 
-                    <Input name="name" icon={FiUser} placeholder="Nome" />
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
+                        <Input name="email" icon={FiMail} placeholder="E-mail" />
+                        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-                    <Button type="submit">Cadastar</Button>
+                        <Button type="submit">Cadastar</Button>
 
-                </Form>
+                    </Form>
 
-                <a href="teste">
-                    <FiArrowLeft />
-                    Voltar para logon
-                </a>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Voltar para logon
+                    </Link>
+                </AnimationContainer>
             </Content>
         </Container>
     );
